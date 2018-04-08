a0a58df3efa009c2fa92cef2ca287e711856ba56
NOswimming <callum.fitt.simpson@gmail.com>
2018-04-08 16:22:29 +1200

    Add hexgrid final assets

---
+++ Assets/Scripts/UI/HexGameUI.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HexGameUI : MonoBehaviour {
+
+	public HexGrid grid;
+
+	HexCell currentCell;
+
+	HexUnit selectedUnit;
+
+    public bool fogOfWarEnabled;
+
+	public void SetEditMode (bool toggle) {
+		enabled = !toggle;
+		grid.ShowUI(!toggle);
+		grid.ClearPath();
+		if (toggle) {
+			Shader.EnableKeyword("HEX_MAP_EDIT_MODE");
+		}
+		else if (fogOfWarEnabled) {
+			Shader.DisableKeyword("HEX_MAP_EDIT_MODE");
+		}
+	}
+
+    public void SetFogOfWar(bool toggle)
+    {
+        fogOfWarEnabled = toggle;
+    }
+
+	void Update () {
+		if (!EventSystem.current.IsPointerOverGameObject()) {
+			if (Input.GetMouseButtonDown(0)) {
+				DoSelection();
+			}
+			else if (selectedUnit) {
+				if (Input.GetMouseButtonDown(1)) {
+					DoMove();
+				}
+				else {
+					DoPathfinding();
+				}
+			}
+		}
+	}
+
+	void DoSelection () {
+		grid.ClearPath();
+		UpdateCurrentCell();
+		if (currentCell) {
+			selectedUnit = currentCell.Unit;
+		}
+	}
+
+	void DoPathfinding () {
+		if (UpdateCurrentCell()) {
+			if (currentCell && selectedUnit.IsValidDestination(currentCell)) {
+				grid.FindPath(selectedUnit.Location, currentCell, selectedUnit);
+			}
+			else {
+				grid.ClearPath();
+			}
+		}
+	}
+
+	void DoMove () {
+		if (grid.HasPath) {
+			selectedUnit.Travel(grid.GetPath());
+			grid.ClearPath();
+		}
+	}
+
+	bool UpdateCurrentCell () {
+		HexCell cell =
+			grid.GetCell(Camera.main.ScreenPointToRay(Input.mousePosition));
+		if (cell != currentCell) {
+            if(currentCell)
+                currentCell.DisableHighlight();
+            currentCell = cell;
+            currentCell.EnableHighlight(Color.blue);
+			return true;
+		}
+		return false;
+	}
+}
